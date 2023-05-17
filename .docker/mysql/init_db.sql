470c9ba6d9a50cbc29fa2a7f03ea1d80dbe4cc61
João Otávio <joaotavioos@outlook.com>
2023-05-17 20:11:12 -0300

    feat: add initial data ✨

---
+++ .docker/mysql/init_db.sql
@@ -0,0 +1,9 @@
+USE nodedb;
+
+CREATE TABLE `people` (
+  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+INSERT INTO `people` (name) VALUES ('Wesley');
